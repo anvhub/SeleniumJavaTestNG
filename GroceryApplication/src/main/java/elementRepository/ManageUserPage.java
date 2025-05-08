4e3635c93485ef90ce16032d54cead6cd435c45c
aruno <aruno@MSI>
2025-05-08 22:58:13 +0530

    5th Commit

---
+++ GroceryApplication/src/main/java/elementRepository/ManageUserPage.java
@@ -0,0 +1,14 @@
+package elementRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.PageFactory;
+
+public class ManageUserPage {
+
+	WebDriver driver;
+
+	public ManageUserPage(WebDriver driver) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+	}
+}
